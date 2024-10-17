5917ad232bc1677b7fcc2823c205d9fe4416a0a8
AliciaPM08 <apachecomena@gmail.com>
2024-10-17 14:01:54 +0200

    ConexionBD terminada

---
+++ Acesso a datos/Proyecto_JDBC/src/ConexionBD.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca-jdbc";
+    private static final String USER = "root";
+    private static final String PW = "";
+
+    //Conexion a la BD
+    public static Connection getConnection(){
+        Connection con=null;
+        try{
+            con = DriverManager.getConnection(URL,USER,PW);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
