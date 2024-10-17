8ad4a93f2def466e60e443debadcd7c36586900c
AliciaPM08 <apachecomena@gmail.com>
2024-10-17 13:38:47 +0200

    Clase Usuario terminada

--- Acesso a datos/Proyecto_JDBC/src/Usuario.java
+++ Acesso a datos/Proyecto_JDBC/src/Usuario.java
@@ -1,4 +1,36 @@
 public class Usuario {
     private String nombre;
-    private String apellido;
+    private int idUsario;
+
+    //Contructor
+    public Usuario(String nombre, int idUsario) {
+        this.nombre = nombre;
+        this.idUsario = idUsario;
+    }
+
+    //Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdUsario() {
+        return idUsario;
+    }
+
+    public void setIdUsario(int idUsario) {
+        this.idUsario = idUsario;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", idUsario=" + idUsario +
+                '}';
+    }
 }
